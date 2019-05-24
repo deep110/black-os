b861e0613e177bfb07b46b7b1f8a2613e3924487
Deepankar Agrawal <deepankar11093@gmail.com>
2019-05-24 18:00:52 +0530

    close qemu as test completes

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,22 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 // implement a custom test runner
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -36,6 +52,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    // end qemu
+    exit_qemu(QemuExitCode::Success);
 }
 
 // test functions
