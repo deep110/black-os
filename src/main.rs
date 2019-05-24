055da98345dcd065594d436e266eca2e623945b5
Deepankar Agrawal <deepankar11093@gmail.com>
2019-05-24 16:41:05 +0530

    add support for test runner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // rename start function in case of test
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -19,5 +23,25 @@ pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+// implement a custom test runner
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+// test functions
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
