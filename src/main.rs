fb0aab636ff85700ed66071b8619166a9eb41579
Deepankar Agrawal <deepankar11093@gmail.com>
2019-05-25 20:51:23 +0530

    add lib for test functions

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@
 #![no_main] // disable all Rust-level entry points
 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(black_os::test_runner)]
 #![reexport_test_harness_main = "test_main"] // rename start function in case of test
 
 use core::panic::PanicInfo;
+use black_os::println;
 
-mod vga_buffer;
-mod serial;
 
 /// This function is called on panic.
 #[cfg(not(test))] // run when not in test mode
@@ -22,10 +21,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    black_os::test_panic_handler(info)
 }
 
 #[no_mangle]
@@ -39,31 +35,4 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-// implement a custom test runner
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    // end qemu
-    exit_qemu(QemuExitCode::Success);
 }
